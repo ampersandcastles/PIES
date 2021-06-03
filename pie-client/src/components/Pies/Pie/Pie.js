3ec3e999a1ab8c5f129c7a58a460d9f9ce3176dc
Tristan Smith <tristan.smith@gmail.com>
2021-06-03 12:03:38 -0500

    lessons

--- pie-client/src/components/Pies/Pie/Pie.js
+++ pie-client/src/components/Pies/Pie/Pie.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import './Pie.css';
+import React from "react";
+import "./Pie.css";
 
-const DisplayPies= (props) => {
-    console.log(props);
+const DisplayPies = (props) => {
+  console.log(props);
 
-    return(
-        <>
-            {
-                props.pie.map((pie, key) => {
-                    return(
-                        <tr key={key}>
-                            <td>{pie.nameOfPie}</td>
-                            <td>{pie.baseOfPie}</td>
-                            <td>{pie.crust}</td>
-                            <td>{pie.timeToBake}</td>
-                            <td>{pie.servings}</td>
-                            <td>{pie.rating}</td>
-                        </tr>
-                    )
-                })
-            }
-        </>
-    )
-}
+  return (
+    <>
+      {props.pie.map((pie, key) => {
+        return (
+          <tr key={key}>
+            <td>{pie.nameOfPie}</td>
+            <td>{pie.baseOfPie}</td>
+            <td>{pie.crust}</td>
+            <td>{pie.timeToBake}</td>
+            <td>{pie.servings}</td>
+            <td>{pie.ratings}</td>
+          </tr>
+        );
+      })}
+    </>
+  );
+};
 
 export default DisplayPies;
--- pie-client/src/components/Pies/Pies.js
+++ pie-client/src/components/Pies/Pies.js
@@ -1,48 +1,66 @@
-import React, {useState, useEffect} from 'react';
-import './Pies.css';
-import DisplayPies from './Pie/Pie';
+import React, { useState, useEffect } from "react";
+import "./Pies.css";
+import DisplayPies from "./Pie/Pie";
 
 const Pies = (props) => {
-    const [pies, setPies] = useState([]);
+  const [pies, setPies] = useState([]);
 
-    const pie = [
-        {
-            nameOfPie: 'pumpkin',
-            baseOfPie: 'pumpkin puree',
-            crust: 'pastry',
-            timeToBake: 50,
-            servings: 8,
-            rating: 4
-        },
-        {
-            nameOfPie: 'apple',
-            baseOfPie: 'candied apples',
-            crust: 'pastry',
-            timeToBake: 40,
-            servings: 4,
-            rating: 3
-        }
-    ];
-    
-    return(
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name of Pie</th>
-                        <th>Base of Pie</th>
-                        <th>Crust</th>
-                        <th>Bake Time</th>
-                        <th>Servings</th>
-                        <th>Rating</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <DisplayPies pie={pie}/>
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  const fetchPies = () => {
+    let url = "http://localhost:3030/pies/";
 
-export default Pies
+    fetch(url, {
+      method: "GET",
+      headers: new Headers({
+        "Content-type": "application/json",
+        Authorization: props.sessionToken,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => setPies(json));
+  };
+
+  useEffect(() => {
+    fetchPies();
+  }, []);
+
+  const pie = [
+    {
+      nameOfPie: "pumpkin",
+      baseOfPie: "pumpkin puree",
+      crust: "pastry",
+      timeToBake: 50,
+      servings: 8,
+      rating: 4,
+    },
+    {
+      nameOfPie: "apple",
+      baseOfPie: "candied apples",
+      crust: "pastry",
+      timeToBake: 40,
+      servings: 4,
+      rating: 3,
+    },
+  ];
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name of Pie</th>
+            <th>Base of Pie</th>
+            <th>Crust</th>
+            <th>Bake Time</th>
+            <th>Servings</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <DisplayPies pie={pies} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Pies;
